4bd9078a14568807b83c41a276deec7288dace1e
benjiwei <benjamin.weidenholzer@gmail.com>
2013-02-23 23:27:35 +0100

    adding first view class

--- MiniJazz/src/com/minijazz/ui/view/MainView.java
+++ MiniJazz/src/com/minijazz/ui/view/MainView.java
@@ -1,5 +1,18 @@
 package com.minijazz.ui.view;
 
-public class MainView {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class MainView {
+	
+	private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(MainView.class);
+
+	public static void main(String[] args) {
+		LOGGER.debug("Test");
+	}
+	
+	public boolean run() {
+		return true;
+	}
+	
 }
---
+++ MiniJazz/test/com/minijazz/ui/view/MainViewTest.java
@@ -0,0 +1,24 @@
+package com.minijazz.ui.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MainViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
